3f1a697f363c78ea91c9094fe7ed5c5c7cd9683f
NsamaChibulu <nsama.chibulu@avanade.com>
2022-04-07 16:58:08 +0100

    loggiing in commands

---
+++ Auto.ps1
@@ -0,0 +1,10 @@
+
+#Config Defined variables
+$Config = Get-Content -Path config.json | ConvertFrom-json
+$UserName = $Config.Username
+$Password = $Congig.Password
+$credential = New-Object System.Management.Automation.PsCredential($UserName, $Password)
+# Login to the Azure console
+Login-AzAccount -Credential $credential
+
+# Connect 
